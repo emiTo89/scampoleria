8e70b86d8fbcd1d576d749f1774670b3eeb66666
Tolu Emiliano (Consulente) <emiliano.tolu@consulenti.sella.it>
2024-11-25 16:48:05 +0100

    updates

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 const EditProductComponent = () => {
   const [error, setError] = useState(false);
