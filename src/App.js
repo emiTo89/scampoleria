aba14e0fdb2d93d0bcbd979d15a3f3236359789b
Tolu Emiliano (Consulente) <emiliano.tolu@consulenti.sella.it>
2024-11-25 14:31:44 +0100

    prova react  1 titolo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Prova react
+          Prova react 1
         </a>
       </header>
     </div>
